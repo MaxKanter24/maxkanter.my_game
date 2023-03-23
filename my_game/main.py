2480e1e408e77141782b7974180d4a151710ebb1
MaxKanter24 <max.kanter24@bcp.org>
2023-03-23 09:39:46 -0700

    first commit

---
+++ my_game/main.py
@@ -0,0 +1,93 @@
+# file created by Max Kanter
+# absent 3/6
+# Code from Chris Cozort
+
+# File created by: Chris Cozort
+# Agenda:
+# gIT GITHUB    
+# Build file and folder structures
+# Create libraries
+# testing github changes
+# I changed something - I changed something else tooooo!
+
+# This file was created by: Chris Cozort
+# Sources: http://kidscancode.org/blog/2016/08/pygame_1-1_getting-started/
+# Sources: 
+
+# import libs
+import pygame as pg
+import random
+import os
+# import settings 
+from settings import *
+from sprites import *
+# from pg.sprite import aSprite
+# set up assets folders
+
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "img")
+
+# create game class in order to pass properties to the sprites file 
+class Game:
+    def __init__(self):
+        #innit game window etc
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("my game")
+        self.clock = pg.time.Clock()
+        self.running = True
+    
+    def new(self):
+        self.score= 0
+        self.all_sprites = pg.sprite.Group()
+        self.platforms = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.player = Player(self)
+        self.all_sprites.add(self.player)
+        for i in range (0, 20):
+            m = Mob(20,20,(0,255,0))
+            self.all_sprites.add(m)
+            self.enemies.add(m)
+        self.run()
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+            self.running = False
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_SPACE:
+                self.player.jump()
+
+    def update(self):
+        self.all_sprites.update()
+    def draw(self):
+        self.screen.fill(BLUE)
+        pg.display.flip()
+        #is this method a function
+    def draw_text(self,text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, RED)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x,y)
+        self.screen.blit(text_surface, text_rect)
+    def get_mouse_now(self):
+        x,y = pg.mouse.get_pos()
+        return (x,y)    
+
+g = Game()
+
+while g.running:
+    g.new()
+
+pg.quit()
---
+++ my_game/settings.py
@@ -0,0 +1,19 @@
+# file created by Max Kanter
+# Absent 3/6
+# Code from Chris Cozort
+
+WIDTH = 800
+HEIGHT = 600
+PLAYER_ACC = 2
+PLAYER_FRICTION = -0.3
+MOB = 2
+MOB_FRICTION = -0.3
+BLACK = (0,0,0)
+BLUE = (50,50,255)
+RED = (255,50,50)
+GREEN = (0,255,0)
+WHITE = (255,255,255)
+FPS = 60
+RUNNING = True
+SCORE = 0
+PAUSE = False
